19f462eb36744d74f43ef4f11439cc71c1bd54bc
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-15 17:50:56 +0900

    (lab4) Hello, Name (lab2 + lab3a)

--- App.tsx
+++ App.tsx
@@ -15,12 +15,31 @@ var input_st = {
 };
 
 const App = () => {
-  const [name, setName] = useState("");
+  const [greet, setGreet] = useState("Hi");
+  const [name, setName] = useState("Name");
 
   return (
     <View style={{ paddingTop: 30 }}>
-      <Text style={text_st}>Hello, {name}</Text>
+      <Text style={text_st}>
+        {greet}, {name}
+      </Text>
       <TextInput style={input_st} onChangeText={setName} />
+
+      <View style={{ margin: 10, flexDirection: "row-reverse" }}>
+        <Button
+          title="Nice"
+          onPress={function () {
+            setGreet("Nice to meet you");
+          }}
+        />
+        <View style={{ width: 10 }}></View>
+        <Button
+          title="Hello"
+          onPress={function () {
+            setGreet("Hello");
+          }}
+        />
+      </View>
     </View>
   );
 };
