66a8e55f462b2df5bfada3cc9e4143b0a9dfd3ed
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-15 17:04:49 +0900

    Add borders. Everything is box

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View, Image, TextInput } from "react-native";
+import { Text, View, Image } from "react-native";
 
 const ICONS = {
   cat1: require("./assets/cat-icon.png"),
@@ -8,13 +8,10 @@ const ICONS = {
 const App = () => {
   return (
     <View style={{ paddingTop: 30 }}>
-      <Text style={{ fontSize: 40, color: "blue" }}>Hello Ewha</Text>
-      <Text>Nice to meet you</Text>
-      <Text style={{ fontSize: 20 }}>Nice to meet you</Text>
-      <Text style={{ fontSize: 30 }}>Nice to meet you</Text>
-      <Image style={{ width: 200, height: 200 }} source={ICONS["cat1"]} />
-      <Image style={{ width: 200, height: 200 }} source={ICONS["cat2"]} />
-      <TextInput style={{ height: 40, borderColor: "gray", borderWidth: 1 }} defaultValue="You can type in me" />
+      <Text style={{ fontSize: 40, borderColor: "red", borderWidth: 1 }}>Hello Ewha</Text>
+      <Text style={{ fontSize: 20, borderColor: "red", borderWidth: 1 }}>Nice to meet you</Text>
+      <Text style={{ fontSize: 30, borderColor: "red", borderWidth: 1 }}>Nice to meet you</Text>
+      <Image style={{ width: 200, height: 200, borderColor: "red", borderWidth: 1 }} source={ICONS["cat1"]} />
     </View>
   );
 };
