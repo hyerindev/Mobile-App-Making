62ab6adfde012ec43b8026ed330f68d95575d11c
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-15 17:14:25 +0900

    (lab 3a) <ScrollView>

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
 
 const ICONS = {
   cat1: require("./assets/cat-icon.png"),
@@ -7,7 +7,7 @@ const ICONS = {
 
 const App = () => {
   return (
-    <View style={{ paddingTop: 30 }}>
+    <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
       <Text style={{ fontSize: 40, color: "blue" }}>Hello Ewha</Text>
       <Text>Nice to meet you</Text>
       <Text style={{ fontSize: 20 }}>Nice to meet you</Text>
@@ -20,7 +20,7 @@ const App = () => {
       <Text style={{ fontSize: 20 }}>Nice to meet you</Text>
       <Text style={{ fontSize: 30 }}>Nice to meet you</Text>
       <Image style={{ width: 200, height: 200 }} source={ICONS["cat1"]} />
-    </View>
+    </ScrollView>
   );
 };
 
