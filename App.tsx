70e4541ef1ad8715a0b60dada9a9e777a2bb90fd
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-15 17:46:52 +0900

    (lab3) TextInput

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, TextInput } from "react-native";
 
 var text_st = {
   fontSize: 30,
@@ -7,29 +7,25 @@ var text_st = {
   padding: 10,
   margin: 10,
 };
+var input_st = {
+  fontSize: 30,
+  borderWidth: 1,
+  padding: 10,
+  margin: 10,
+};
 
 const App = () => {
-  const [greet, setGreet] = useState("Hi");
+  const [name, setName] = useState("");
 
   return (
     <View style={{ paddingTop: 30 }}>
-      <Text style={text_st}>{greet}</Text>
-      <Text style={text_st}>{greet}</Text>
-      <View style={{ margin: 10, flexDirection: "row-reverse" }}>
-        <Button
-          title="Press Me"
-          onPress={function () {
-            setGreet("Hello, Ewha");
-          }}
-        />
-        <View style={{ width: 10 }}></View>
-        <Button
-          title="Touch Me"
-          onPress={function () {
-            setGreet("Nice to meet you");
-          }}
-        />
-      </View>
+      <Text style={text_st}>Hello, {name}</Text>
+      <TextInput
+        style={input_st}
+        onChangeText={function (t) {
+          setName(t);
+        }}
+      />
     </View>
   );
 };
