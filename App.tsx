c6fb5d416c654cc05c6a0c6e2acd84162508ae50
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-22 17:58:14 +0900

    (lab2) ScrollView can be nested - Netflix style

--- App.tsx
+++ App.tsx
@@ -1,16 +1,58 @@
-import { View, ScrollView } from "react-native";
+import type { TextStyle, ImageStyle } from "react-native";
+import { View, ScrollView, Text, Image } from "react-native";
+
+const IMAGES_JS = [
+  require("./assets/js-1.png"),
+  require("./assets/js-2.png"),
+  require("./assets/js-3.png"),
+  require("./assets/js-4.png"),
+];
+const IMAGES_MOVIE = [
+  require("./assets/movie-1.png"),
+  require("./assets/movie-2.png"),
+  require("./assets/movie-3.png"),
+  require("./assets/movie-4.png"),
+  require("./assets/movie-5.png"),
+  require("./assets/movie-6.png"),
+  require("./assets/movie-7.png"),
+  require("./assets/movie-8.png"),
+  require("./assets/movie-9.png"),
+];
+
+const textStyle: TextStyle = {
+  fontSize: 24,
+  fontWeight: "bold",
+  color: "white",
+};
+const imageStyle: ImageStyle = {
+  width: 200,
+  height: 300,
+};
 
 const App = () => {
   return (
-    <View style={{ paddingTop: 30 }}>
-      <ScrollView horizontal style={{ margin: 10 }}>
-        <View style={{ backgroundColor: "red", width: 100, height: 100 }} />
-        <View style={{ backgroundColor: "blue", width: 100, height: 100 }} />
-        <View style={{ backgroundColor: "green", width: 100, height: 100 }} />
-        <View style={{ backgroundColor: "lightgray", width: 100, height: 100 }} />
-        <View style={{ backgroundColor: "yellow", width: 100, height: 100 }} />
-      </ScrollView>
-    </View>
+    <ScrollView
+      contentContainerStyle={{ gap: 24, padding: 10, paddingVertical: 50 }}
+      style={{ backgroundColor: "black" }}
+    >
+      <View style={{ width: "100%", height: 300, backgroundColor: "red" }} />
+      <View style={{ gap: 8 }}>
+        <Text style={textStyle}>주술회전</Text>
+        <ScrollView horizontal contentContainerStyle={{ gap: 8 }}>
+          {IMAGES_JS.map((item, index) => (
+            <Image key={index} source={item} style={imageStyle} />
+          ))}
+        </ScrollView>
+      </View>
+      <View style={{ gap: 8 }}>
+        <Text style={textStyle}>박스오피스</Text>
+        <ScrollView horizontal contentContainerStyle={{ gap: 8 }}>
+          {IMAGES_MOVIE.map((item, index) => (
+            <Image key={index} source={item} style={imageStyle} />
+          ))}
+        </ScrollView>
+      </View>
+    </ScrollView>
   );
 };
 
