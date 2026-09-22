5440aa5da65dd24a5869e380f34e3debbb8b2173
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-22 20:21:35 +0900

    (lab 6) State in various place

--- App.tsx
+++ App.tsx
@@ -1,38 +1,21 @@
 import type { TextStyle } from "react-native";
 import { useState } from "react";
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, Button } from "react-native";
 
-const commonStyle: TextStyle = {
-  padding: 10,
-  fontSize: 24,
-};
 const textStyle: TextStyle = {
-  ...commonStyle,
+  padding: 10,
   backgroundColor: "lightgray",
-};
-const inputStyle: TextStyle = {
-  ...commonStyle,
-  borderWidth: 1,
 };
 
 const App = () => {
-  const [val, setVal] = useState<number>(0);
-  const [step, setStep] = useState<number>(5);
+  const [fontSize, setFontSize] = useState<number>(24);
 
   return (
     <View style={{ padding: 12, paddingTop: 36, gap: 20 }}>
-      <Text style={textStyle}>{val}</Text>
-      <View>
-        <Text style={{ fontSize: 28, fontWeight: "bold" }}>STEP</Text>
-        <TextInput style={inputStyle} onChangeText={(input) => setStep(Number(input))} />
-      </View>
+      <Text style={{ ...textStyle, fontSize: fontSize }}>Using State in Style!</Text>
       <View style={{ gap: 8 }}>
-        <Button title="+1" onPress={() => setVal((prev) => prev + 1)} />
-        <Button title="-1" onPress={() => setVal((prev) => prev - 1)} />
-      </View>
-      <View style={{ gap: 8 }}>
-        <Button title={"+" + step} onPress={() => setVal((prev) => prev + step)} />
-        <Button title={"-" + step} onPress={() => setVal((prev) => prev - step)} />
+        <Button title="+1" onPress={() => setFontSize((prev) => prev + 1)} />
+        <Button title="-1" onPress={() => setFontSize((prev) => prev - 1)} />
       </View>
     </View>
   );
