a4ac9a61e40d7c75bb60d1fe5e6318c39e94ec6c
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-22 19:55:04 +0900

    (lab 5) TextInput으로 더하기

--- App.tsx
+++ App.tsx
@@ -1,27 +1,39 @@
 import type { TextStyle } from "react-native";
 import { useState } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 
-const textStyle: TextStyle = {
+const commonStyle: TextStyle = {
   padding: 10,
+  fontSize: 24,
+};
+const textStyle: TextStyle = {
+  ...commonStyle,
   backgroundColor: "lightgray",
-  fontSize: 30,
+};
+const inputStyle: TextStyle = {
+  ...commonStyle,
+  borderWidth: 1,
 };
 
 const App = () => {
   const [val, setVal] = useState<number>(0);
+  const [nb, setNb] = useState<number>(0);
 
   return (
-    <View style={{ padding: 12, paddingTop: 36, gap: 20 }}>
+    <View style={{ padding: 12, paddingTop: 36, gap: 16 }}>
       <Text style={textStyle}>{val}</Text>
-      <View style={{ flexDirection: "row", gap: 8 }}>
-        <Button title="+1" onPress={() => setVal((prev) => prev + 1)} />
-        <Button title="-1" onPress={() => setVal((prev) => prev - 1)} />
-      </View>
-      <View style={{ flexDirection: "row", gap: 8 }}>
-        <Button title="+5" onPress={() => setVal((prev) => prev + 5)} />
-        <Button title="-5" onPress={() => setVal((prev) => prev - 5)} />
-      </View>
+      <TextInput
+        style={inputStyle}
+        onChangeText={(input) => {
+          setNb(Number(input));
+        }}
+      />
+      <Button
+        title="PLUS"
+        onPress={() => {
+          setVal(val + nb);
+        }}
+      />
     </View>
   );
 };
