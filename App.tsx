f8f0e6f02094f9d6257b8fb2395ec03ee6fc62ca
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-22 18:11:37 +0900

    (lab3) Counting Characters

--- App.tsx
+++ App.tsx
@@ -1,58 +1,30 @@
-import type { TextStyle, ImageStyle } from "react-native";
-import { View, ScrollView, Text, Image } from "react-native";
+import type { TextStyle } from "react-native";
+import { useState } from "react";
+import { View, Text, TextInput } from "react-native";
 
-const IMAGES_JS = [
-  require("./assets/js-1.png"),
-  require("./assets/js-2.png"),
-  require("./assets/js-3.png"),
-  require("./assets/js-4.png"),
-];
-const IMAGES_MOVIE = [
-  require("./assets/movie-1.png"),
-  require("./assets/movie-2.png"),
-  require("./assets/movie-3.png"),
-  require("./assets/movie-4.png"),
-  require("./assets/movie-5.png"),
-  require("./assets/movie-6.png"),
-  require("./assets/movie-7.png"),
-  require("./assets/movie-8.png"),
-  require("./assets/movie-9.png"),
-];
-
-const textStyle: TextStyle = {
+const commonStyle: TextStyle = {
+  margin: 10,
+  padding: 10,
   fontSize: 24,
-  fontWeight: "bold",
-  color: "white",
 };
-const imageStyle: ImageStyle = {
-  width: 200,
-  height: 300,
+const textStyle: TextStyle = {
+  ...commonStyle,
+  backgroundColor: "lightgray",
+};
+const inputStyle: TextStyle = {
+  ...commonStyle,
+  borderWidth: 1,
 };
 
 const App = () => {
+  const [name, setName] = useState<string>("");
+
   return (
-    <ScrollView
-      contentContainerStyle={{ gap: 24, padding: 10, paddingVertical: 50 }}
-      style={{ backgroundColor: "black" }}
-    >
-      <View style={{ width: "100%", height: 300, backgroundColor: "red" }} />
-      <View style={{ gap: 8 }}>
-        <Text style={textStyle}>주술회전</Text>
-        <ScrollView horizontal contentContainerStyle={{ gap: 8 }}>
-          {IMAGES_JS.map((item, index) => (
-            <Image key={index} source={item} style={imageStyle} />
-          ))}
-        </ScrollView>
-      </View>
-      <View style={{ gap: 8 }}>
-        <Text style={textStyle}>박스오피스</Text>
-        <ScrollView horizontal contentContainerStyle={{ gap: 8 }}>
-          {IMAGES_MOVIE.map((item, index) => (
-            <Image key={index} source={item} style={imageStyle} />
-          ))}
-        </ScrollView>
-      </View>
-    </ScrollView>
+    <View style={{ paddingTop: 30 }}>
+      <Text style={textStyle}>Hello, {name}</Text>
+      <TextInput style={inputStyle} onChangeText={setName} />
+      <Text style={{ fontSize: 12, margin: 10 }}>글자 수: {("Hello, " + name).length}</Text>
+    </View>
   );
 };
 
