4d52f1a4ccccc79e537691bf70f1e271d366c70d
Hyerin Sung <hyerinx@ewha.ac.kr>
2026-09-22 17:30:49 +0900

    (lab7) 구구단 App (2)

--- App.tsx
+++ App.tsx
@@ -3,12 +3,7 @@ import { TextStyle, Text, View, Button } from "react-native";
 
 const text_var: TextStyle = {
   padding: 10,
-  textAlign: "center",
   backgroundColor: "lightgray",
-  fontSize: 30,
-};
-const text_static: TextStyle = {
-  padding: 10,
   fontSize: 30,
 };
 
@@ -17,40 +12,40 @@ const App = () => {
   const [b, setB] = useState(0);
 
   return (
-    <View style={{ overflow: "visible", paddingTop: 30, flexDirection: "row", gap: 5 }}>
-      <View style={{ overflow: "visible", width: 80, gap: 20 }}>
-        <Text style={text_var}>{a}</Text>
-        <Button
-          title="+"
-          onPress={function () {
-            setA((prev) => prev + 1);
-          }}
-        />
-        <Button
-          title="-"
-          onPress={function () {
-            setA((prev) => prev - 1);
-          }}
-        />
+    <View style={{ overflow: "visible", paddingTop: 30, gap: 20 }}>
+      <Text style={text_var}>
+        {a} × {b} ＝ {a * b}
+      </Text>
+      <View style={{ flexDirection: "row", gap: 20 }}>
+        <View style={{ width: 50, gap: 20 }}>
+          <Button
+            title="+"
+            onPress={() => {
+              setA((prev) => prev + 1);
+            }}
+          />
+          <Button
+            title="-"
+            onPress={() => {
+              setA((prev) => prev - 1);
+            }}
+          />
+        </View>
+        <View style={{ width: 50, gap: 20 }}>
+          <Button
+            title="+"
+            onPress={() => {
+              setB((prev) => prev + 1);
+            }}
+          />
+          <Button
+            title="-"
+            onPress={() => {
+              setB((prev) => prev - 1);
+            }}
+          />
+        </View>
       </View>
-      <Text style={text_static}>×</Text>
-      <View style={{ overflow: "visible", width: 80, gap: 20 }}>
-        <Text style={text_var}>{b}</Text>
-        <Button
-          title="+"
-          onPress={function () {
-            setB((prev) => prev + 1);
-          }}
-        />
-        <Button
-          title="-"
-          onPress={function () {
-            setB((prev) => prev - 1);
-          }}
-        />
-      </View>
-      <Text style={text_static}>＝</Text>
-      <Text style={{ ...text_var, height: 60 }}>{a * b}</Text>
     </View>
   );
 };
